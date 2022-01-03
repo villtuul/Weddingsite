8fb815fdb48c55030a23b35f5409e6452ba6944f
villtuul <ville.tuulio@hotmail.com>
2022-01-03 21:52:26 +0200

    Added styled-components instead of using app.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import backgroundImage from './background.jpeg';
 import './App.css';
-
+import Tuuliot from './components/tuuliot';
 
 function App() {
-  return (
-    <>
-      <div class="st-image"></div>
-      <h1 class= "st-text st-title">
-        <h1>Event</h1>
-        <div class="form__group">
-          <input type="text" class="form__input" id="name" placeholder="Code" required="" onChange={(e) => {this.handleChange(e)}}/>
-          <label for="name" class="form__label">Code</label>
-        </div>
-      </h1>
-    </>
-
-  );
-}
+  return (<Tuuliot/>);}
 
 export default App;
