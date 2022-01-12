b35040b57c4bb68787a419d76e021ec46edc3bd7
villtuul <ville.tuulio@hotmail.com>
2022-01-12 21:16:24 +0200

    Added location images

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,5 +1,6 @@
 import * as translation from "../translations/en.json";
 import LanguageSelector from "./LanguageSelector";
+import route from "../route.png";
 
 const ContentPage = ({locale, setValue, setIsLogin, setLocale}) => {
     return (
@@ -8,6 +9,9 @@ const ContentPage = ({locale, setValue, setIsLogin, setLocale}) => {
             <p>{translation[locale].text}</p>
             <h2>{translation[locale].location}</h2>
             <p>{translation[locale].text}</p>
+            <img src={"https://www.senaatti.fi/app/uploads/2017/05/1_extraLarge_865_-1024x825.jpg"} alt={"Turun linna"}/>
+            <img src={"https://files.venuu.se/attachments/000/001/987/d89719bda40805e0a32931065b3d29e8615a2ab6.jpg"} alt={"Manilla"}/>
+            <img src={route} alt={"Route from castle to Manilla"}/>
             <h2>{translation[locale].registration}</h2>
 
             <button onClick={() => {
