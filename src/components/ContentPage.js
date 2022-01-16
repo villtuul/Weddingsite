ec4a7b482c9e1afd060d1c148b8a0bbaa77c1b88
villtuul <ville.tuulio@hotmail.com>
2022-01-16 20:01:49 +0200

    Updated some dependiec due to security vulnerabilities

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,6 +1,7 @@
 import * as translation from "../translations/en.json";
 import LanguageSelector from "./LanguageSelector";
 import route from "../route.png";
+import "../App.css"
 
 const ContentPage = ({locale, setValue, setIsLogin, setLocale}) => {
     return (
@@ -8,10 +9,10 @@ const ContentPage = ({locale, setValue, setIsLogin, setLocale}) => {
             <h1>{translation[locale].title}</h1>
             <p>{translation[locale].text}</p>
             <h2>{translation[locale].location}</h2>
-            <p>{translation[locale].text}</p>
-            <img src={"https://www.senaatti.fi/app/uploads/2017/05/1_extraLarge_865_-1024x825.jpg"} alt={"Turun linna"}/>
-            <img src={"https://files.venuu.se/attachments/000/001/987/d89719bda40805e0a32931065b3d29e8615a2ab6.jpg"} alt={"Manilla"}/>
-            <img src={route} alt={"Route from castle to Manilla"}/>
+            <p><img src={"https://www.senaatti.fi/app/uploads/2017/05/1_extraLarge_865_-1024x825.jpg"} alt={"Turun linna"}/>
+                <img src={"https://files.venuu.se/attachments/000/001/987/d89719bda40805e0a32931065b3d29e8615a2ab6.jpg"} alt={"Manilla"}/>
+                <img src={route} alt={"Route from castle to Manilla"}/>
+                {translation[locale].text}</p>
             <h2>{translation[locale].registration}</h2>
 
             <button onClick={() => {
