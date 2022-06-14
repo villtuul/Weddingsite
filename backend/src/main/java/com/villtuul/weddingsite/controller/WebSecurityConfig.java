cfab2295d652938ac6e7c7c94fdaf63ec6e34b07
villtuul <ville.tuulio@hotmail.com>
2022-06-14 14:47:23 +0300

    Ui changes and proper database connection in production

--- backend/src/main/java/com/villtuul/weddingsite/controller/WebSecurityConfig.java
+++ backend/src/main/java/com/villtuul/weddingsite/controller/WebSecurityConfig.java
@@ -6,24 +6,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Configuration
--- frontend/src/components/ContentPage.js
+++ frontend/src/components/ContentPage.js
@@ -1,24 +1,26 @@
 import * as translation from "../translations/translation.json";
 import RegistrationForm from "./RegistrationForm";
-import route from "../images/route.png";
+import Parser from 'html-react-parser';
 import "../App.scss";
-import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
 
 const ContentPage = ({locale}) => {
+    let poem = translation[locale].text5;
     return (
         <div className="contentbox loggedin">
             <p>{translation[locale].text1}</p>
-            <h1>{translation[locale].title}</h1>
-            <p>{"24.09.2022"}</p>
-            <p>{translation[locale].time}</p>
+            <div className="contenttitle">
+                <h1>{translation[locale].title}</h1>
+                24.09.2022
+                <br/>{translation[locale].time}
+            </div>
             <p>{translation[locale].text2}</p>
             <p>{translation[locale].text3}</p>
             <h2>{translation[locale].info}</h2>
             <p>{translation[locale].text7}</p>
             <p>{translation[locale].text4}</p>
             <p>{translation[locale].text8}</p>
-            <p>{translation[locale].text5}</p>
+            <div className="poem">{Parser(poem)}</div>
             <p>{translation[locale].text6}</p>
             <h2>{translation[locale].registration}</h2>
             <RegistrationForm locale={locale}/>
